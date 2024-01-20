ff0e54769ac373c469cfbf7f1376aefb8cec93d1
Aleksander <aleksve@gmail.com>
2024-01-20 21:39:35 +0100

    (1) Write numbers to the allocated sequence (2) syscall nanosleep for five seconds (3) Minor changes

--- main.asm
+++ main.asm
@@ -18,12 +18,18 @@ mov r8, rax
 ; allocate mem with mmap
 %define ENOMEM 12
 %define buflen 1000
+%define buflen 1000*1000*1000  ; Intentionally cause ENOMEM
+%define buflen 100*1000*1000*1000  ; Intentionally cause ENOMEM
 mov rax, 9
 mov rdi, 0
-mov rsi, 10000000000000; Intentionally cause ENOMEM
 mov rsi, buflen
-mov rdx, 2; prot=PROT_WRITE
-mov r10, 2; flags=MAP_PRIVATE
+
+%define PROT_WRITE 0x2; https://github.com/torvalds/linux/blob/master/include/linux/mman.h
+mov rdx, PROT_WRITE
+
+%define MAP_ANONYMOUS 0x20
+%define MAP_PRIVATE 0x2; https://github.com/torvalds/linux/blob/c25b24fa72c734f8cd6c31a13548013263b26286/include/uapi/linux/mman.h#L18
+mov r10, MAP_ANONYMOUS + MAP_PRIVATE; flags=MAP_PRIVATE
 syscall
 
 cmp rax, -ENOMEM
@@ -35,6 +41,24 @@ syscall
 alloc_OK:
 mov r9, rax
 
+
+mov rax, 0
+mov r10, buflen
+loop:
+mov [rax+r9], rax
+add rax, 8
+cmp rax, r10
+jne loop
+
+; Sleep
+%define syscall_nanosleep 0x23
+mov rax, syscall_nanosleep
+push dword 5; Seconds
+push dword 0; Millisecond
+mov rdi, rsp
+add rdi, 8
+mov rsi, 0
+syscall
 
 mov rax, 0
 mov rdi, r8
@@ -62,6 +86,6 @@ ret
 
 section .data
 message:  db "Hello, World!", 10
-cannot: db "Cannot allocate!", 10
+cannot: db "Couldn't allocate the requested memory!", 10
 cannot_len: equ $-cannot
 filename: db "./Makefile", 0
